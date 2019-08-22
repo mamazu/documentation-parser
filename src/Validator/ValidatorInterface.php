35fc90259a8767d8d7907747a9913e14c2ee3a92
mamazu <14860264+mamazu@users.noreply.github.com>
2019-08-22 23:45:09 +0200

    Adding doc comments

--- src/Validator/ValidatorInterface.php
+++ src/Validator/ValidatorInterface.php
@@ -8,5 +8,10 @@ use Mamazu\DocumentationParser\Parser\Block;
 
 interface ValidatorInterface
 {
+    /**
+     * Validates a block of code and returns an array of errors
+     * 
+     * @return Error[]
+     */
     public function validate(Block $block): array;
 }
